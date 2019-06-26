9700e2c8f03794cf87feeb1de717c6ff0cdfc7e3
Fredrik Furtenbach <fredrik@furtenbach.se>
2019-06-26 10:06:40 +0200

    Fix #2

--- core/hal-bundle.js
+++ core/hal-bundle.js
@@ -5,21 +5,19 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         this.name = config.name;
         this.bundleset = config.bundleset;
-        this.outputValue = config.outputValue;
-        this.outputType = config.outputType;
 
         var node = this;
 
         node.on('input', function(msg) {
             for (var i = 0; i < this.bundleset.length; i += 1) {
                 var bundle = node.bundleset[i];
-                var item = RED.nodes.getNode(bundle);
+                var item = RED.nodes.getNode(bundle.item);
 
                 msg.topic = item.topic ? item.topic : msg.topic;
                 msg.id = item.identifier ? item.identifier : msg.id;
                 msg.name = item.name;
 
-                msg = utils.generateMsg[node.outputType](msg,node.outputValue,item);
+                msg = utils.generateMsg[bundle.type](msg,bundle.value,item);
                 node.send(msg);
             }
         });
