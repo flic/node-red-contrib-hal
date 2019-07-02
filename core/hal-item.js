a4f4c372c81230c41967512ae6db750746adfbc7
Fredrik Furtenbach <fredrik@Fredriks-MBP.lan>
2019-07-02 15:17:16 +0200

    Fix #11

--- core/hal-item.js
+++ core/hal-item.js
@@ -12,12 +12,34 @@ module.exports = function(RED) {
         this.propertyType = config.propertyType;
         this.outputValue = config.outputValue;
         this.outputType = config.outputType;
+        this.bootstrap = config.bootstrap;
+        this.bootstrapValue = config.bootstrapValue;
+        this.bootstrapType = config.bootstrapType;
         this.events = RED.nodes.getNode(config.config);
 
         var node = this;
         utils.showState(node,null);
 
+        // Send bootstrap output
+        if (node.bootstrap === true) {
+            setTimeout(function() {
+                node.emit("input",null);
+            },100);
+        }
+
         node.on('input', function(msg) {
+            if (msg == null) {
+                if (node.output === true) {
+                    var msg = {};
+                    msg.payload = RED.util.evaluateNodeProperty(node.bootstrapValue,node.bootstrapType,node);
+                    msg._msgid = RED.util.generateId();
+                    msg.topic = node.topic ? node.topic : null;
+                    msg.id = node.identifier ? node.identifier : null;
+                    node.send(msg);
+                }
+                return;
+            }
+
             if (node.filter === true && msg.topic !== node.topic) {
                 node.debug("msg.topic doesn't match configured value and filter is enabled. Dropping message.");
                 return;
@@ -40,7 +62,6 @@ module.exports = function(RED) {
                 msg.payload = utils.generatePayload[node.outputType](msg,node.outputValue,node.state);
                 node.send(msg);
             }
-
         });
     }
     RED.nodes.registerType("item",halItem);
